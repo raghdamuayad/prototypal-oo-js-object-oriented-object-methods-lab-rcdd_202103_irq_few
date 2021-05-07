e89c3b3e25dfa689c2844af8e03c173a8cc4dc1d
Raghda  Muayad <therose000777@gmail.com>
2021-05-07 09:12:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,51 @@
+
+function BoardMember(name, homestate, training) {
+  this.name = name;
+    this.homestate = homestate;
+  this.training =training;
+  this.sayHi = function() {
+      return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;
+    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+
+    let  polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+
+  }
+
+  this.releasePressStatement = function() {
+      return `You will see great things from Scuber.`;
+    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+
+    let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+
+  }
+
+
+
+  this.doCharity = function() {
+      return `I like to help people.`;
+    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+
+    let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+
+  }
+  this.approve() = function() {
+      return `You can do that!`;
+    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+
+    let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+
+  }
+  }
+//    veto();{
+
+  //    if (polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law"))
+
+    //        console.log(`No, I must disagree`);
+      //  }
+
+    //  approve();{
+
+
+              //  return `You can do that!`;
+          //  }
+          //  let  polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
