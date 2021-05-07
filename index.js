11d1c46115bc7f84d561fd95571556f1914bc29c
Raghda  Muayad <therose000777@gmail.com>
2021-05-07 09:17:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function BoardMember(name, homeState, training){
    this.veto = function(){
      return 'No, I must disagree'
      }
-     this.approve = function(){
+  this.approve = function(){
    return 'You can do that!'
      }
-    this.doCharity = function(){
-     return "I like to help people."
+  this.doCharity = function(){
+   return "I like to help people."
       }
 
         this.releasePressStatement = function(){
