24cf26c5b152670a16155bccb5aa872f06e366e9
Raghda  Muayad <therose000777@gmail.com>
2021-05-07 09:16:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,51 +1,24 @@
 
-function BoardMember(name, homestate, training) {
-  this.name = name;
-    this.homestate = homestate;
-  this.training =training;
-  this.sayHi = function() {
-      return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;
-    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
 
-    let  polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+   this.veto = function(){
+     return 'No, I must disagree'
+     }
+     this.approve = function(){
+   return 'You can do that!'
+     }  
+    this.doCharity = function(){
+     return "I like to help people."
+      }
 
+        this.releasePressStatement = function(){
+         return "You will see great things from Scuber."
+      }
+   
+      this.sayHi = function(){
+       return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  } 
   }
-
-  this.releasePressStatement = function() {
-      return `You will see great things from Scuber.`;
-    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
-
-    let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
-
-  }
-
-
-
-  this.doCharity = function() {
-      return `I like to help people.`;
-    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
-
-    let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
-
-  }
-  this.approve() = function() {
-      return `You can do that!`;
-    //  Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
-
-    let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
-
-  }
-  }
-//    veto();{
-
-  //    if (polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law"))
-
-    //        console.log(`No, I must disagree`);
-      //  }
-
-    //  approve();{
-
-
-              //  return `You can do that!`;
-          //  }
-          //  let  polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
